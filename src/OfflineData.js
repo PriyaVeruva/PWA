97ae1ab6cdce1f25938363408774433be585e9ea
deepak <deepak@emproto.com>
2022-06-28 15:35:47 +0530

    intial commit

--- src/OfflineData.js
+++
@@ -1,9 +0,0 @@
-import { Offline, Online } from "react-detect-offline";
-
-const OfflineData = () => (
-  <div>
-    <Online>Only shown when you're online</Online>
-    <Offline>Only shown offline (surprise!)</Offline>
-  </div>
-);
-export default OfflineData
